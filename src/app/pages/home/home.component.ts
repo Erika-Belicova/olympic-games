5f7b9c96226cbb8b4e5ec5adf0efc578e58fbcb2
Erika Belicova <146355532+Erika-Belicova@users.noreply.github.com>
2025-02-05 19:40:04 +0100

    Installed ngx-charts library and added basic pie chart configuration in HomeComponent

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import { OlympicCountry } from 'src/app/core/models/Olympic';
+import { single } from 'src/app/pages/home/data';
 
 @Component({
   selector: 'app-home',
@@ -11,9 +12,35 @@ import { OlympicCountry } from 'src/app/core/models/Olympic';
 export class HomeComponent implements OnInit {
   public olympics$: Observable<OlympicCountry[] | null> = of(null); // added type
 
-  constructor(private olympicService: OlympicService) {}
+  // configuration for ngx-charts
+  single: any[] = [];
+  view: [number, number] = [700, 400];
+
+  // options
+  gradient: boolean = false;
+  showLegend: boolean = false;
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
+  legendPosition: string = 'below';
+  colorScheme: string = 'cool';
+
+  constructor(private olympicService: OlympicService) {
+    Object.assign(this, { single });
+  }
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+  }
+  
+  onSelect(data: any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data: any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data: any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
 }
