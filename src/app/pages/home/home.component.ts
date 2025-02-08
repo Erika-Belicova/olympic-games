4050e825cc5b6b34d9955f1d316318320b0cf5da
Erika Belicova <146355532+Erika-Belicova@users.noreply.github.com>
2025-02-09 00:21:19 +0100

    Replace testing data with pie chart data in home.component.html and remove testing data file and references

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,7 +3,6 @@ import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import { OlympicCountry } from 'src/app/core/models/Olympic';
 import { Participation } from 'src/app/core/models/Participation';
-import { single } from 'src/app/pages/home/data';
 import { map } from 'rxjs/operators';
 import { catchError } from 'rxjs/operators';
 
@@ -17,7 +16,6 @@ export class HomeComponent implements OnInit {
   public pieChartData$: Observable<{ name: string; value: number }[]> = of([]); // observable for pie chart data
 
   // configuration for ngx-charts
-  single: any[] = [];
   view: [number, number] = [700, 400];
 
   // options for ngx-charts
@@ -28,9 +26,7 @@ export class HomeComponent implements OnInit {
   legendPosition: string = 'below';
   colorScheme: string = 'cool';
 
-  constructor(private olympicService: OlympicService) {
-    Object.assign(this, { single });
-  }
+  constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
