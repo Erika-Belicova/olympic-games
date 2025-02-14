f0b7ca230cc407c3ec8cb0ea06566cc47a81647b
Erika Belicova <146355532+Erika-Belicova@users.noreply.github.com>
2025-02-14 15:24:14 +0100

    Update onSelect method in HomeComponent to use route parameter, adjust DetailComponent path in AppRoutingModule, and import ActivatedRoute in DetailComponent

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrl: './detail.component.scss'
 })
-export class DetailComponent {
+export class DetailComponent implements OnInit {
 
+  countryName: string = '';
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.countryName = this.route.snapshot.params['name'];
+  }
 }
