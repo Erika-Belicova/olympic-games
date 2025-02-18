c348e70a74e8746f7773655fa323a2d8c4b520c8
Erika Belicova <146355532+Erika-Belicova@users.noreply.github.com>
2025-02-18 11:59:07 +0100

    Add methods showErrorSnackBar and handleObservableError for error handling, add try-catch to onSelect method, add observables for loading and error

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import { OlympicCountry } from 'src/app/core/models/Olympic';
-import { faAward } from '@fortawesome/free-solid-svg-icons'; 
+import { faAward } from '@fortawesome/free-solid-svg-icons';
 import { Router } from '@angular/router';
+import { catchError, delay, tap } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-home',
@@ -11,9 +13,15 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public numberOfJOs$: Observable<number | null> = of(0); // observable for number of JOs
+  public numberOfJOs$: Observable<number | null> = of(null); // observable for number of JOs
   public olympics$: Observable<OlympicCountry[] | null> = of(null); // observable for raw data
-  public pieChartData$: Observable<{ name: string; value: number }[] | null> = of([]); // observable for pie chart data
+  public pieChartData$: Observable<{ name: string; value: number }[] | null> = of(null); // observable for pie chart data
+
+  public error$: Observable<string | null> = of(null); // observable for errors
+  private loadingState = new BehaviorSubject<boolean>(false);
+  public loading$ = this.loadingState.asObservable();
+
+  errorMessage: string = "";
 
   // configuration for ngx-charts
   gradient: boolean = false;
@@ -28,17 +36,71 @@ export class HomeComponent implements OnInit {
   medalIcon = faAward; // medal icon from awesome fonts
 
   constructor(private olympicService: OlympicService, 
-              private router: Router) {}
+              private router: Router,
+              private snackBar: MatSnackBar) {}
 
   ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
-    this.numberOfJOs$ = this.olympicService.fetchNumberOfJOs();
-    this.pieChartData$ = this.olympicService.fetchData();
+  
+    this.olympics$ = this.handleObservableError(
+      this.olympicService.getOlympics(),
+      'Failed get Olympic data.',
+      null
+    );
+
+    this.numberOfJOs$ = this.handleObservableError(
+      this.olympicService.fetchNumberOfJOs(),
+      'Failed to get the number of JOs.',
+      null
+    );
+
+    this.pieChartData$ = this.handleObservableError(
+      this.olympicService.fetchData(),
+      'Failed to get the pie chart data.',
+      null
+    );
+
+    this.loading$ = this.olympicService.loading.pipe(delay(500)); // with 0.5 sec delay
+    this.error$ = this.olympicService.error;
   }
 
   onSelect(data: { name: string, value: number }): void {
     console.log('Item clicked', JSON.parse(JSON.stringify(data)));
     // on click the country name is used as a route paramter to navigate to detail page
-    this.router.navigateByUrl(`detail/${data.name}`);
+    try {
+      this.router.navigateByUrl(`detail/${data.name}`);
+    } catch (error) {
+      console.error('Error while trying to navigate to detail page:', error);
+      this.snackBar.open('An error occurred while trying to open details. ', 'Close', {
+        duration: 5000,
+        horizontalPosition: 'right',
+        verticalPosition: 'top',
+        panelClass: ['error-snackbar'],
+      });
+    }
+  }
+
+  private handleObservableError<T>(observable$: Observable<T>, errorMessage: string, fallbackValue: T): Observable<T> {
+    this.loadingState.next(true); // start loading
+    return observable$.pipe(
+      catchError(() => {
+        this.errorMessage = errorMessage;
+        this.showErrorSnackBar(errorMessage);
+        this.loadingState.next(false); // end loading
+        return of(fallbackValue);
+      }),
+      tap(() => { 
+        this.loadingState.next(false); // end loading
+        this.errorMessage = ''; // reset error message
+      })
+    );
+  }
+  
+  showErrorSnackBar(message: string): void {
+    this.snackBar.open(message, 'Close', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      panelClass: ['error-snackbar'],
+    });
   }
 }
